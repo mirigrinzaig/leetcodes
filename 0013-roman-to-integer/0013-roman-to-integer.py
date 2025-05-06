7a23fce9e61f85933f6325bddbea5b4b3d1f5f1e
Miriam Grinzaig <154724177+mirigrinzaig@users.noreply.github.com>
2025-05-06 13:34:02 +0300

    Time: 6 ms (75.37%), Space: 12.5 MB (54.34%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def romanToInt(self, s):
+        dictionary = {"I": 1, "V": 5, "X": 10, "L": 50,
+                      "C": 100, "D": 500, "M": 1000}
+        total = 0
+        prev_value = 0
+        
+        for ch in reversed(s):
+            value = dictionary[ch]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+                prev_value = value
+        return total
