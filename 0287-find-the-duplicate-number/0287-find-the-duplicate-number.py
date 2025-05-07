cc3a4b1593fdc8b06f40a1c816551970dbf52c84
Miriam Grinzaig <154724177+mirigrinzaig@users.noreply.github.com>
2025-05-07 14:04:17 +0300

    Time: 0 ms (100%), Space: 21 MB (55.86%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,10 +1,6 @@
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution(object):
     def findDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         slow = nums[0]
         fast = nums[0]
         while True:
