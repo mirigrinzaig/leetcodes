d50b7791a31f5e0e5a1384b915e543c9bc544843
Miriam Grinzaig <154724177+mirigrinzaig@users.noreply.github.com>
2025-07-16 17:55:27 +0300

    Time: 10 ms (27.44%), Space: 15.8 MB (27.67%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        from collections import defaultdict
+
+        graph = defaultdict(list)
+        for a, b in prerequisites:
+            graph[b].append(a)
+
+        result = []
+        visited = [0] * numCourses 
+        has_cycle = [False] 
+
+        def dfs(node):
+            if visited[node] == 1:
+                has_cycle[0] = True
+                return
+            if visited[node] == 2:
+                return
+
+            visited[node] = 1 
+            for neighbor in graph[node]:
+                dfs(neighbor)
+            visited[node] = 2 
+            result.append(node)
+
+        for course in range(numCourses):
+            if visited[course] == 0:
+                dfs(course)
+            if has_cycle[0]:
+                return []
+
+        return result[::-1]
